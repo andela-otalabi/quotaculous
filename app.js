433514f8f64689ffd418f5b571dfaf4418f7bae8
unknown <oatalabi@gmail.com>
2015-05-12 16:50:59 +0100

    added dislike and like buttons

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var app = angular.module('root', ['LocalStorageModule']);
 
+//app.run(function(editableOptions) {
+//  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+//});
+
 app.controller('main', ['$scope', 'localStorageService', function($scope, localStorageService) {
-  $scope.quoteList = localStorageService.get('quotes') || [];
+  $scope.quoteList = localStorageService.get('quotes');
+  $scope.loveQs = false;
+  $scope.showQs = false;
 
   $scope.submit = function() {
     var quote = {
       'name': $scope.quoteName,
       'author': $scope.author,
-      'category': $scope.category
+      'category': $scope.category,
+      ratings: {
+        'likes': 0,
+        'dislikes': 0
+      }
     };
+
     $scope.quoteList.push(quote);
     localStorageService.set('quotes', $scope.quoteList);
     //clears input field on submit
@@ -18,11 +29,32 @@ app.controller('main', ['$scope', 'localStorageService', function($scope, localS
   };
 
   $scope.delete = function(index) {
-    var delQuote = confirm("Are you sure you want to edit?");
+    var delQuote = confirm("Are you sure you want to delete?");
     if (delQuote === true) {
       $scope.quoteList.splice(index, 1);
       localStorageService.set('quotes', $scope.quoteList);
     }
+  };
+
+  $scope.like = function(index) {
+    $scope.quoteList[index].ratings.likes += 1;
+    localStorageService.set('quotes', $scope.quoteList);
+  };
+
+  $scope.dislike = function(index) {
+    $scope.quoteList[index].ratings.dislikes += 1;
+    localStorageService.set('quotes', $scope.quoteList);
+  };
+
+  $scope.toggleLove = function() {
+    $scope.loveQuotes = [];
+    $scope.q = localStorageService.get('quotes');
+    for (var i = 0; i < $scope.q.length; i++) {
+      if ($scope.q[i].category === "love") {
+        $scope.loveQuotes.push($scope.q[i]);
+      }
+    }
+    $scope.loveQs = !$scope.loveQs;
   };
 
   $scope.load = function() {
@@ -34,6 +66,7 @@ app.controller('main', ['$scope', 'localStorageService', function($scope, localS
     for (var i = 0; i < $scope.data.length; i++) {
       $scope.categories.push($scope.data[i].category);
     }
+    $scope.showQs = !$scope.showQs;
     // $scope.allCategories = 
     console.log($scope.categories);
     // $scope.categoryData = localStorageService.get('category');
